9feaa6ba9c8a39e2b1530588857f95b872876f76
ellenjia <50282305+Noah-jia@users.noreply.github.com>
2022-10-18 15:42:27 +0800

    pull commit
    测试本地拉去远程仓库

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test!");
         System.out.println("推送一下试试");
+        System.out.println("测试一下拉取代码");
     }
 }
